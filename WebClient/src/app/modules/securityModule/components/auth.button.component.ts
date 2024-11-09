8022862f33764c729631e3a2184d6d5fd8e42e95
BillKrat <billkrat@adventuresOnTheEdge.net>
2024-11-09 17:50:44 -0600

    Mobile app stying

--- WebClient/src/app/modules/securityModule/components/auth.button.component.ts
+++ WebClient/src/app/modules/securityModule/components/auth.button.component.ts
@@ -10,17 +10,11 @@ import { environment } from '../../../../environments/environment';
     <ng-container *ngIf="auth.isAuthenticated$ | async; else loggedOut">
       <a class="pill" rel="noopener" (click)="auth.logout({ logoutParams: { returnTo: returnToUrl } })">
         &nbsp;Log out&nbsp;
-        <svg xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 -960 960 960" width="14" fill="currentColor">
-          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"  />
-        </svg>
       </a>
     </ng-container>
     <ng-template #loggedOut>
       <a class="pill"  rel="noopener" (click)="auth.loginWithRedirect()">
         &nbsp;Log in&nbsp;
-        <svg xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 -960 960 960" width="14" fill="currentColor">
-          <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z"  />
-        </svg>
       </a>
     </ng-template>
   `,
