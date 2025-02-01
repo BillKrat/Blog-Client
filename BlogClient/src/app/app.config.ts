bc8b42705374f869a9d510d79adf548300f74bd7
BillKrat <billkrat@adventuresOnTheEdge.net>
2025-01-31 19:43:31 -0600

    NgPrime and new Angular app

--- BlogClient/src/app/app.config.ts
+++ BlogClient/src/app/app.config.ts
@@ -1,8 +1,32 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { 
+  ApplicationConfig, 
+  provideZoneChangeDetection 
+} from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+      provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),
+      provideAnimationsAsync(),
+      providePrimeNG({
+        // ripple: true,
+        theme: {
+          preset: Aura,
+          options: {
+            prefix: 'p',
+            // https://primeng.org/theming customization
+            darkModeSelector: '.my-app-dark',
+            cssLayer: {
+              name: 'primeng',
+              order: 'app-styles, primeng, another-css-library'
+            }
+          }
+        }
+      }),
+
+    ]
 };
